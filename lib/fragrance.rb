b6991b6b5f036fb3321074c9e37fe98d71155757
Michael Lorant <mikel@mlvision.com.au>
2015-04-05 12:11:27 +1000

    Added Thor as the CLI parser

--- lib/fragrance.rb
+++ lib/fragrance.rb
@@ -1,4 +1,5 @@
 require 'fragrance/version'
+require 'fragrance/commandline'
 require 'fragrance/core'
 require 'fragrance/ec2'
 require 'fragrance/elb'
@@ -13,11 +14,15 @@ module Fragrance
       @elb = Fragrance::Elb.new
     end
 
-    def run
-      ARGV.each do |instance_id|
+    def version
+      puts Fragrance::VERSION
+    end
+
+    def refresh(instances)
+      instances.each do |instance_id|
         @elb.find_instance(instance_id).each do |load_balancer|
           if check(load_balancer, instance_id)
-            refresh(load_balancer, instance_id)
+            reregister(load_balancer, instance_id)
           end
         end
       end
@@ -31,8 +36,8 @@ module Fragrance
         (@ec2.state(instance_id) == 'running')
     end
 
-    def refresh(load_balancer, instance_id)
-      puts "Refreshing #{load_balancer}: #{instance_id}"
+    def reregister(load_balancer, instance_id)
+      puts "Reregistering #{load_balancer}: #{instance_id}"
       @elb.reregister_instance(load_balancer, instance_id)
     end
   end
---
+++ lib/fragrance/commandline.rb
@@ -0,0 +1,27 @@
+require 'thor'
+
+module Fragrance
+  # Fragrance::Commandline
+  class Commandline < Thor
+    package_name 'Fragrance'
+    map ['-v', '--version'] => :version
+
+    desc 'version', 'Print the version and exit.'
+
+    def version
+      Fragrance::App.new.version
+    end
+
+    desc 'refresh', 'Refresh stopped instances in all load balancers.'
+
+    method_option :instances,
+                  type:     :array,
+                  aliases:  '-i',
+                  required: true,
+                  desc: 'List of instances separated by spaces.'
+
+    def refresh
+      Fragrance::App.new.refresh(options[:instances])
+    end
+  end
+end
